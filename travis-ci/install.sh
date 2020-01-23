d5883f3431c2237c6fff6321bb80f0d8771d544b
Norman Kämper-Leymann <norman.kaemper@aperto.com>
2020-01-23 12:52:12 +0100

    Apply https://www.drupal.org/files/issues/2020-01-22/3048293-10.patch.

--- travis-ci/install.sh
+++ travis-ci/install.sh
@@ -10,21 +10,15 @@ rm .git -rf
 
 # Create our main Drupal project.
 echo "# Creating Drupal project"
-composer create-project drupal-composer/drupal-project:8.x-dev ${DRUPAL_BUILD_ROOT}/drupal --stability dev --no-interaction --no-install
+composer create-project drupal/drupal-recommended-project:${DRUPAL_CORE} ${DRUPAL_BUILD_ROOT}/drupal --stability dev --no-interaction
 cd ${DRUPAL_BUILD_ROOT}/drupal
 
 # Set our drupal core version.
-composer require --no-update drupal/core ${DRUPAL_CORE}
 composer require --dev --no-update drupal/coder wimg/php-compatibility jakub-onderka/php-parallel-lint jakub-onderka/php-console-highlighter
-
-# We do not need drupal console and drush (required by drupal-project) for tests.
-composer remove drupal/console drush/drush --no-update
 
 # Add our repositories for gdpr, as well as re-adding the Drupal package repo.
 echo "# Configuring package repos"
 composer config repositories.0 path ${TRAVIS_BUILD_DIR}
-composer config repositories.1 composer https://packages.drupal.org/8
-composer config extra.enable-patching true
 
 # Now require contacts which will pull itself from the paths.
 echo "# Requiring gdpr"
